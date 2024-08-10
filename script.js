12af659f65715484e022244e8eae4ad4b3c8c5d9
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-08-10 23:20:28 +0300

    Make code prettier

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const mobileMenu = document.querySelector('.nav__mobile-menu');
-const hamburgerIcon = document.querySelector('.nav__hamburger');
-const closeIcon = document.querySelector('.nav__closemenu');
-const navbar = document.querySelector('.nav');
+const mobileMenu = document.querySelector(".nav__mobile-menu");
+const hamburgerIcon = document.querySelector(".nav__hamburger");
+const closeIcon = document.querySelector(".nav__closemenu");
+const navbar = document.querySelector(".nav");
 
-hamburgerIcon.addEventListener('click', ()=> {
-    mobileMenu.classList.add('active');
-    navbar.style.overflow = 'visible';
+hamburgerIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+  navbar.style.overflow = "visible";
 });
-closeIcon.addEventListener('click', ()=> {
-    mobileMenu.classList.remove('active');
-    navbar.style.overflow = 'hidden';
+closeIcon.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  navbar.style.overflow = "hidden";
 });
