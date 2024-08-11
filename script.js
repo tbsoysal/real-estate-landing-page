6b077503be5e32f253f920af2916e4b2d47b34b5
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-08-12 00:20:16 +0300

    Recreate navigation bar

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const mobileMenu = document.querySelector(".nav__mobile-menu");
-const hamburgerIcon = document.querySelector(".nav__hamburger");
-const closeIcon = document.querySelector(".nav__closemenu");
-const navbar = document.querySelector(".nav");
+const navMenu = document.querySelector('.nav__menu');
+const navButtons = document.querySelector('.nav__buttons');
+const navToggleBtn = document.querySelector('.nav__toggleBtn');
 
-hamburgerIcon.addEventListener("click", () => {
-  mobileMenu.classList.add("active");
-  navbar.style.overflow = "visible";
-});
-closeIcon.addEventListener("click", () => {
-  mobileMenu.classList.remove("active");
-  navbar.style.overflow = "hidden";
-});
+function showMobileMenu() {
+  navMenu.classList.add('show');
+  navButtons.classList.add('show');
+  navToggleBtn.classList.add('close');
+}
+
+function hideMobileMenu() {
+  navMenu.classList.remove('show');
+  navButtons.classList.remove('show');
+  navToggleBtn.classList.remove('close');
+}
+
+navToggleBtn.addEventListener('click', () => {
+  isVissible = navToggleBtn.classList.contains('close');
+  if(isVissible) {
+    hideMobileMenu();
+  }else {
+    showMobileMenu();
+  }
+})
